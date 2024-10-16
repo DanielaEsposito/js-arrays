ae37c213fca0075182e4e1ca797ca31866d50626
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-10-16 18:40:09 +0200

    prova risoluzione alternativa es 3

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -18,8 +18,27 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const nameLenght = i.lenght(<= 5);
+for (let i=0; i < teachers; i++){
+  let nameOfTeacher;
+  if(nameLenght === teachers[i]){
+
+  }
+}
+const longNames = [];
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const nameSerched ='Ed'
+for (let i=0; i < teachers; i++){
+  let nameOfTeacher;
+  if(nameSerched === teachers[i]){
+   nameSerched = null;
+  }
+  
+  
+}
+
+
+
 teachers.splice(1,1);
 console.log(teachers);
